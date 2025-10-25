571e0707703c6898a0b66608028bc2c689da8c35
Marthala Harshavardhan Reddy <106993835+HXR-Git@users.noreply.github.com>
2025-10-25 17:18:02 +0530

    logo removed
    Comment out the page icon in the Streamlit configuration.

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ logging.basicConfig(level=logging.INFO, format="%(levelname)s:root:%(message)s")
 # --- PAGE CONFIGURATION ---
 st.set_page_config(
     page_title="Finance Dashboard",
-    page_icon="pages/logo.png",
+    #page_icon="pages/logo.png",
     layout="wide",
     initial_sidebar_state="collapsed"
 )
